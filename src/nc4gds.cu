4ebc563625aaab5cef75c8285f8b03b85ce8e456
Yogesh Singla <yogeshsingla481@gmail.com>
2026-02-14 10:46:56 +0530

    file name

--- src/nc4gds.cu
+++ src/nc4gds.cu
@@ -194,7 +194,7 @@ auto main(const int32_t argc, const char* const* const argv) -> int32_t {
     close(fd);
 
   } else {
-    std::cerr << "Warning: Failed to open file for cache drop: " << name << std::endl;
+    std::cerr << "Warning: Failed to open file for cache drop: " << filename << std::endl;
   }
   //
   // BENCHMARK: NetCDF-4 READ
